55e090079cd24b1b65c5d3286ed43616b5876a83
Andriy Kotovych <manul.andriy@gmail.com>
2017-03-14 17:03:29 +0200

    Adds omniauth-facebook authentification

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,6 +248,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, '1654256341545621', '08a439dfd05be35a95a86bd3c305aa62'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   get 'home/index'
-
-  devise_for :users
   root to: 'home#index'
+
+  devise_for :users,
+             controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
