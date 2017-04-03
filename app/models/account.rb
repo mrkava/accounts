f702bf3777c7ba322f3dbb0ddac65ce55cf8da84
Andriy Kotovych <manul.andriy@gmail.com>
2017-04-04 02:18:43 +0300

    Adds status to account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,8 @@
 class Account < ApplicationRecord
   belongs_to :user
 
+  enum status: [:opened, :auction, :sold]
+
   validates :bookmaker, :age, presence: true
   validates :bookmaker, inclusion: { in: Appvalues['bookmaker'] }
   validates :age, inclusion: { in: 1..180 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328115930) do
+ActiveRecord::Schema.define(version: 20170403230145) do
 
   create_table "accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "bookmaker"
     t.integer  "age"
     t.boolean  "own"
     t.text     "comment",    limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.integer  "user_id"
+    t.integer  "status",                   default: 0
     t.index ["user_id"], name: "index_accounts_on_user_id", using: :btree
   end
 
